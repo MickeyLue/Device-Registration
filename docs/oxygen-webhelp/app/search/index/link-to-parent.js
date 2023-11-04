be9d15e8a12c85ace0f5846c2d6c2a094b1b7b2e
Mickey Lue <luess007@umn.edu>
2023-11-04 07:19:58 -0500

    Edits

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,2 +1,2 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-var linkToParent = {12:9,8:15,9:-1,11:9,14:9,7:8,3:8,13:9,6:5,16:15,15:-1,5:-1,2:14,10:12,17:14,4:13};
+var linkToParent = {14:9,8:15,16:15,17:14,13:9,3:8,7:8,15:-1,2:14,9:-1,5:-1,10:12,6:5,12:9,4:13,11:9};
